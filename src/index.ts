8a151e26bd4a716c9eea07e5f1578561b9583642
Bayram Ali Başgül <bayraak@gmail.com>
2019-08-07 09:44:27 +0000

    Feature/last gitlab ci cd integration

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,6 @@ createConnection().then(async connection => {
     const port = process.env.PORT || 3000;
 
     app.listen(port, () => {
-        console.log(`process.env.PORT = ${process.env.PORT}`);
         console.log(`Server started on port ${port}`);
     });
 
